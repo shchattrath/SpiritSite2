6aac284aa5557b80b294561a045173d40185c4d9
shchattrath <shchattrath@gmail.com>
2023-07-26 09:26:22 -0700

    input sql progress

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
-import mysql from 'mysql'
+import mysql from "mysql2"
 
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password: "shaan",
+    password: "12345",
     database: "blog"
 })
--- spiritblog/src/components/pages/register.js
+++ spiritblog/src/components/pages/register.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useState} from 'react'
 
 const Register = () => {
+    const [inputs,setInputs] = useState({
+        email:"",
+        username:"",
+        password:"", 
+    }) 
+
+    const handleChange = e =>{
+        setInputs(prev =>({...prev, [e.target.name]: e.target.value}))
+    }
+
+    console.log(inputs)
+
     return(
         <div className = 'auth'>
             
             <h1>Register</h1>
             <form>
-                <input  className='textbox' type = "text" placeholder ='username'/>
-                <input  className='textbox' type = "text" placeholder = 'email'/>
-                <input  className='textbox' type = "text" placeholder ='password'/>
+                <input  name = 'username' className='textbox' type = "text" placeholder ='username' onChange = {handleChange}/>
+                <input  name = 'email' className='textbox' type = "text" placeholder = 'email' onChange = {handleChange}/>
+                <input  name = 'password' className='textbox' type = "text" placeholder ='password' onChange = {handleChange}/>
                 <p>An error has occurred, please try again.</p>
                 <button className='textbox' >Register</button>
                 <span><Link className='link' to="/login">Already have an account?</Link> </span>
